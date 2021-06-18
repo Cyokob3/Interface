80d2e8f668a238f11a97a78b03aada6e7222cd6a
Cyokob3 <c6102010@planet.kanazawa-it.ac.jp>
2021-06-18 18:16:48 +0900

    時間で並び替えを行えるようにする

--- corpus_test.py
+++ corpus_test.py
@@ -1,14 +1,22 @@
-#class Textcorpus():
-
 def getcorpus():
     fname = "/Users/sugimotoyoshiki/prog/python/Interface/data/G3.txt"
+    i = 0
+    corpus = []
     if fname == '': return
     f = open(fname,'r')
     lines = f.readlines()
     f.close()
     for line in lines:
-        l = [line.strip() for line in line.split("\t")]
-        #print(l[2])
-        print(l)
+        l = line.split("\t")
+        i += 1
+        l[1] = str(i)
+        corpus.append(l)
+        time = []
+    time = [(float((corpus[i][2])), corpus[i][1]) for i in range(len(corpus))]
+    sort_time = sorted(time)
+    t1 = [sort_time[i][1] for i in range(len(sort_time))]
+    print(t1)
+            
+    
 
 getcorpus()
